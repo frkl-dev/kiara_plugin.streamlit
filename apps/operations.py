1cdf33738ab0bcea892f4ea10f98e6db92121e27
Markus Binsteiner <markus@frkl.io>
2023-01-14 13:09:47 +0000

    refactor: rename cut_column -> pick.column

--- apps/operations.py
+++ apps/operations.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 
 import streamlit as st
 
@@ -7,5 +8,16 @@ import kiara_plugin.streamlit as kst
 st.set_page_config(layout="wide")
 
 kst.init()
+st.kiara.api.set_active_context("_operation_doc", create=True)
+
+if "corpus_files" not in st.kiara.api.get_alias_names():
+    with st.spinner("Downloading example data ..."):
+        pipeline_file = os.path.join(
+            os.path.dirname(__file__), "pipelines", "operations_doc_onboarding.yaml"
+        )
+        results = st.kiara.api.run_job(pipeline_file)
+
+        for field_name, value in results.items():
+            st.kiara.api.store_value(value, field_name)
 
 st.kiara.operation_documentation()
